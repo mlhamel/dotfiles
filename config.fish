f6c6bec96344b411d2451be39205f322f61f4810
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2016-10-04 10:18:34 -0400

    Adds fish configuration

---
+++ config.fish
@@ -0,0 +1,52 @@
+if test -f /opt/dev/dev.fish
+  source /opt/dev/dev.fish
+end
+
+# Alias
+alias ls "ls -G"
+alias e "emacs"
+alias ec "emacsclient -n"
+alias hlogin "env HEROKU_ORGANIZATION=shopify heroku login --sso"
+
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+function fish_prompt
+  set last_status $status
+
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
+  set_color normal
+
+  printf '%s ' (__fish_git_prompt)
+
+  if set -q VIRTUAL_ENV
+    echo -n -s (set_color -b black yellow ) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+  end
+
+  set_color normal
+end
+
+## Python stuff
+eval (python -m virtualfish)
